08e5f3df742fdd2e23dc876753d07be0ef2f2d1f
Felipe Telles <felipe.melo.telles@gmail.com>
2024-02-05 21:28:03 -0300

    working led

--- Firmware/main/main.c
+++ Firmware/main/main.c
@@ -11,8 +11,10 @@
 #include "tinyusb.h"
 #include "class/hid/hid_device.h"
 #include "driver/gpio.h"
+#include "led_strip.h"
 
 #define APP_BUTTON (GPIO_NUM_0) // Use BOOT signal by default
+#define BLINK_GPIO GPIO_NUM_48
 static const char *TAG = "example";
 
 /************* TinyUSB descriptors ****************/
@@ -84,6 +86,43 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_
 {
 }
 
+/********* LED ***************/
+
+static led_strip_handle_t led_strip;
+static uint8_t s_led_state = 0;
+
+static void blink_led(void)
+{
+    /* If the addressable LED is enabled */
+    if (s_led_state)
+    {
+        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        /* Refresh the strip to send data */
+        led_strip_refresh(led_strip);
+    }
+    else
+    {
+        /* Set all LED off to clear all pixels */
+        led_strip_clear(led_strip);
+    }
+}
+
+static void configure_led(void)
+{
+    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
+    /* LED strip initialization with the GPIO and pixels number*/
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = BLINK_GPIO,
+        .max_leds = 1, // at least one LED on board
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000, // 10MHz
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    /* Set all LED off to clear all pixels */
+    led_strip_clear(led_strip);
+}
 /********* Application ***************/
 
 static void app_send_hid_demo(void)
@@ -120,6 +159,8 @@ void app_main(void)
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
     ESP_LOGI(TAG, "USB initialization DONE");
 
+    configure_led();
+
     while (1)
     {
         if (tud_mounted())
@@ -128,9 +169,12 @@ void app_main(void)
             if (send_hid_data)
             {
                 app_send_hid_demo();
+                blink_led();
+                s_led_state = !s_led_state;
             }
             send_hid_data = !gpio_get_level(APP_BUTTON);
         }
         vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
+
